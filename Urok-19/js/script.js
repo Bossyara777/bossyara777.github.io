ca5dbeae02dbfea8f10627160ad4b92b36091cc1
Bossyara777 <52579815+Bossyara777@users.noreply.github.com>
2021-02-13 14:28:26 +0200

    Добавлено ДЗ к уроку 19

---
+++ Urok-19/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    <!-- Табы на JS -->
+    // $(".dws-form").on("click", ".tab", function () {
+    //    Удаляем классы active
+    //     $(".dws-form").find(".active").removeClass("active");
+
+    //    Добавляем класс active
+    //     $(this).addClass("active");
+    //     $(".tab-form").eq($(this).index()).addClass("active");
+    // });
+});
